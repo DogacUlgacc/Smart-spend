31b1ccf18d6885f674b14c96cc6ed2cb649489da
Doğaç Ulğaç <ulgac1907@gmail.com>
2026-01-24 16:44:40 +0300

    feat: update configurations and enhance user service functionality
    - Updated springdoc-openapi dependency version in product-service and added it to user-service.
    - Refactored UserController to include new endpoints for deleting a user and retrieving user details.
    - Introduced UserResponseMapper for mapping User entities to UserResponse DTOs.

--- user-service/src/main/java/com/dogac/user_service/web/UserController.java
+++ user-service/src/main/java/com/dogac/user_service/web/UserController.java
@@ -1,7 +1,12 @@
 package com.dogac.user_service.web;
 
+import java.util.List;
 import java.util.UUID;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -10,11 +15,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dogac.user_service.application.bus.CommandBus;
+import com.dogac.user_service.application.bus.QueryBus;
 import com.dogac.user_service.application.commands.CreateUserCommand;
+import com.dogac.user_service.application.commands.DeleteUserCommand;
 import com.dogac.user_service.application.commands.UpdateUserCommand;
 import com.dogac.user_service.application.dto.CreatedUserResponse;
 import com.dogac.user_service.application.dto.UpdateUserRequest;
 import com.dogac.user_service.application.dto.UpdatedUserResponse;
+import com.dogac.user_service.application.dto.UserResponse;
+import com.dogac.user_service.application.queries.GetAllUsersQuery;
+import com.dogac.user_service.application.queries.GetUserByIdQuery;
 
 import jakarta.validation.Valid;
 
@@ -22,18 +32,21 @@ import jakarta.validation.Valid;
 @RequestMapping("/api/v1/user")
 public class UserController {
     private final CommandBus commandBus;
+    private final QueryBus queryBus;
 
-    public UserController(CommandBus commandBus) {
+    public UserController(CommandBus commandBus, QueryBus queryBus) {
         this.commandBus = commandBus;
+        this.queryBus = queryBus;
     }
 
     @PostMapping
-    public CreatedUserResponse createUser(@RequestBody CreateUserCommand command) {
-        return commandBus.send(command);
+    public ResponseEntity<CreatedUserResponse> createUser(@RequestBody @Valid CreateUserCommand command) {
+        CreatedUserResponse response = commandBus.send(command);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
     @PutMapping("/{id}")
-    public UpdatedUserResponse updateUser(
+    public ResponseEntity<UpdatedUserResponse> updateUser(
             @PathVariable UUID id,
             @RequestBody @Valid UpdateUserRequest request) {
 
@@ -43,8 +56,30 @@ public class UserController {
                 request.lastName(),
                 request.phoneNumber(),
                 request.userType());
+        UpdatedUserResponse response = commandBus.send(command);
+        return ResponseEntity.ok(response);
 
-        return commandBus.send(command);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(
+            @PathVariable UUID id) {
+        commandBus.send(new DeleteUserCommand(id));
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserResponse> getUserById(@PathVariable UUID id) {
+        GetUserByIdQuery query = new GetUserByIdQuery(id);
+        UserResponse response = queryBus.execute(query);
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping()
+    public ResponseEntity<List<UserResponse>> getUsers() {
+        GetAllUsersQuery query = new GetAllUsersQuery();
+        List<UserResponse> responseList = queryBus.execute(query);
+        return ResponseEntity.ok(responseList);
     }
 
 }
